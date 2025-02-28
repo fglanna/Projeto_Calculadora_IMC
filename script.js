620f52a03d8e8821933a9e99fcb67bbe3a29a3b9
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2025-02-28 14:42:05 -0300

    logica JS

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const formulario = document.getElementById("calc-form");
+  const nomeInput = document.getElementById("nome");
+  const nomeError = document.getElementById("nome-error");
+
+  formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (!nomeInput.value) {
+      nomeError.style.display = "block";
+      return;
+    } else {
+      nomeError.style.display = "none";
+    }
+
+    const nome = nomeInput.value;
+    const altura = parseFloat(document.getElementById("altura").value);
+    const peso = parseFloat(document.getElementById("peso").value);
+    
+    const imc = peso / (altura * altura)
+    const resultElement = document.getElementById('resultado')
+
+    let categoria;
+
+    if(imc < 18.5) {
+        categoria = "Abaixo do Peso"
+        resultElement.style.backgroundColor = "#f9ff00"
+    } else if (imc < 25) {
+        categoria = "Peso Normal"
+        resultElement.style.backgroundColor = "#00FF00"
+    } else if (imc < 30) {
+        categoria = "Sobrepeso"
+        resultElement.style.backgroundColor = "#d58a24"
+    } else if (imc < 35) {
+        categoria = "Obesidade Grau I"
+        resultElement.style.backgroundColor = "#b26c18"
+    } else if (imc < 40) {
+        categoria = "Obesidade Grau II"
+        resultElement.style.backgroundColor = "#f8230f"
+    } else {
+        categoria = "Obesidade Mórbida"
+        resultElement.style.backgroundColor = "#dd1a0b"
+    }
+
+    resultElement.innerHTML = `<p> ${nome}, seu IMC é ${imc.toFixed(1)}</p>
+    <p>Você esta na categoria: ${categoria}</p>`
+
+  });
+});
